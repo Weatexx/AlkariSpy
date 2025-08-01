b71f5c64714b1a988c72c6254c5554c4214173b1
Koray <ardakoraykartal@gmail.com>
2025-08-01 23:11:36 +0300

    General Update
    CLI'dan çalışması için güncellemeler yapıldı

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='alkarimenu',  # Komut ismi
+    version='0.1',  # Proje versiyonu
+    description='An OSINT tool for searching usernames and personal data across various platforms using Google Dorking.',
+    author='None',  # Yazar adı
+    author_email='None',  # Yazar e-posta adresi
+    url='https://github.com/Weatexx/AlkariSpy',  # Proje URL'si
+    packages=find_packages(),  # Proje paketlerini bulur
+    install_requires=[  # Gereksinimler
+        'requests',
+        'googlesearch-python',
+        'colorama',
+        'pyfiglet',
+        'keyboard',  # veya 'pynput' kullanabilirsiniz
+    ],
+    entry_points={  # Komut satırında çalıştırılabilir yapmak için
+        'console_scripts': [
+            'alkarimenu = main:run_osint_tool',  # main.py içindeki 'run_osint_tool' fonksiyonunu çalıştır
+        ],
+    },
+    classifiers=[  # Python projeniz için sınıflandırmalar
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
